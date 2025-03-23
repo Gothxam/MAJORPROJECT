f66f7e4bfc4c0b52a27c27c4c301bbbc32f42de0
Nikhil kundra <nikhilkundra0@gmail.com>
2025-03-23 21:10:36 +0530

    Updated files and added new routes

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const session= require("express-session")
-const flash=require("connect-flash");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-
-
-const listings = require("./routes/listing.js"); //51.1. listing.js required
-const reviews = require("./routes/review.js"); //51.2 review.js required
+const listingRouter = require("./routes/listing.js"); //51.1. listing.js required
+const reviewRouter = require("./routes/review.js"); //51.2 review.js required
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/easybnb";
 
@@ -35,33 +37,50 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 // session establisshed
-const sessionOption={
+const sessionOption = {
   secret: "mysupersecretstring",
   resave: false,
   saveUninitialized: true,
-  cookie:{
-    expires:Date.now()+ 7 * 24 * 60 * 60 * 1000,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
   },
 };
 
 app.get("/", (req, res) => {
-  res.send("hi, im root");
+  res.send("Hi, I am root");
 });
 
 app.use(session(sessionOption));
 app.use(flash());
 
-app.use((req, res, next)=>{
-  res.locals.success=req.flash("success");
-    res.locals.error=req.flash("error");
-  
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "delta-student",
+//   });
+
+//   let registeredUser = await User.register(fakeUser, "helloworld");
+//   res.send(registeredUser);
+// });
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found !")); //worng page req err
--- schema.js
+++ schema.js
@@ -1,22 +1,25 @@
-const joi= require("joi");
+const joi = require("joi");
 const review = require("./models/review");
 
-
-module.exports.listingSchema =joi.object({
-  listing : joi.object().required({
-      title : joi.string().required(),
-      description : joi.string().required(),
-      price : joi.number().required(),
-      country : joi.string().required(),
-      location : joi.string().required().min(0),
-      image : joi.string().allow("",null)
-
-  }).required(),
+module.exports.listingSchema = joi.object({
+  listing: joi
+    .object()
+    .required({
+      title: joi.string().required(),
+      description: joi.string().required(),
+      price: joi.number().required(),
+      country: joi.string().required(),
+      location: joi.string().required().min(0),
+      image: joi.string().allow("", null),
+    })
+    .required(),
 });
 
-module.exports.reviewSchema =joi.object({
-  review: joi.object({
-    rating:joi.number().required().min(1).max(5),
-    Comment:joi.string().required(),
-  }).required(),
+module.exports.reviewSchema = joi.object({
+  review: joi
+    .object({
+      rating: joi.number().required().min(1).max(5),
+      Comment: joi.string().required(),
+    })
+    .required(),
 });
