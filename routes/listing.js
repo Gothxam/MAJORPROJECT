4fb8b35d2f018d492071751b40468b0b98b5de8a
Gautam <gautamkholi410@gmail.com>
2025-03-24 13:19:40 +0530

    review authorization done

--- routes/listing.js
+++ routes/listing.js
@@ -27,12 +27,19 @@ router.get(
   "/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews").populate("owner");
+    const listing = await Listing.findById(id)
+    .populate({
+      path:"reviews",
+      populate:{
+        path:"author",
+      },
+     })
+     .populate("owner");
     if(!listing){
       req.flash("error"," Listing you requested for doesn't exist !");
       res.redirect("/listings")
     }
-    console.log(listing);
+
     
     res.render("listings/show.ejs", { listing });
   })
--- routes/review.js
+++ routes/review.js
@@ -5,17 +5,20 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview}=require("../middleware.js")
+const {validateReview, isLoggedIn,isReviewAuthor}=require("../middleware.js")
 
 
 // 51.2. reviews route pasted from app.js
 //Post Review Route
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author=req.user._id;
+    
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
@@ -26,6 +29,8 @@ router.post(
 //Delete Review Route
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
 
